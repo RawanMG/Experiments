4cfc38907f874c79af8d0d686adafa2e02dbf55b
RawanMG <rawan.alghofaili@gmail.com>
2014-10-21 14:23:56 -0400

    fixed is_inside() + implemented pnpoly()

--- roadmap.py
+++ roadmap.py
@@ -46,44 +46,60 @@ def num_intersect(point, polygon):
 	if np.linalg.norm(N) != 0.0:
 		N = N/np.linalg.norm(N)
 	return N
+
+
+def inside_poly(point, poly):
+	size = len(poly)
+	for i in range(0, size):
+		p1 = np.array([poly[i][0], poly[i][1], 0]) # (x1, y1) in line l1
+		p2 = np.array([poly[(i+1)%size][0], poly[(i+1)%size][1], 0]) # (x2, y2) in line l2
+		l1 = p2-p1
+		pline = np.array(point)-p1
+
+		if i-1 <= 0:
+			p2 = np.array([poly[size-1][0], poly[size-1][1], 0])
+		else:
+			p2 = np.array([poly[i-1][0], poly[i-1][1], 0])
+		l2 = p2-p1
+			
+		cp1 = np.cross(l1, l2)
+		cp2 = np.cross(l1, pline)
+
+		if np.dot(cp1, cp2) < 0:
+			return False
+
+	return True
+
 def is_inside(point, polygons):
-	print point
 	for poly in polygons:
-		size = len(poly)
-		for i in range(0, size):
-			p1 = np.array([poly[i][0], poly[i][1], 0]) # (x1, y1) in line l1
-			p2 = np.array([poly[(i+1)%size][0], poly[(i+1)%size][1], 0]) # (x2, y2) in line l2
-			l1 = p2-p1
-			print l1
-			print p2
-			print p1
+		if inside_poly(point, poly):
+			return True
 
-			if i-1 <= 0:
-				p2 = np.array([poly[size-1][0], poly[size-1][1], 0])
-			else:
-				p2 = np.array([poly[i-1][0], poly[i-1][1], 0])
-			l2 = p2-p1
-			print l2
-			print p2
-			pline = np.array(point)-p1
-			cp1 = np.cross(l1, l2)
-			cp1 = cp1
-			print cp1
-			cp2 = np.cross(l1, pline)
-			cp2 = cp2
-			print pline
-			print cp2
-			#sys.exit(0)	
-			print np.dot(cp1, cp2)
-			if np.dot(cp1, cp2) < 0:
-				return False
-
-	return True		
+	return False
 
 
+
+def pnpoly(xp, yp, x, y):
+	""" References: 
+		http://erich.realtimerendering.com/ptinpoly/
+		http://www.faqs.org/faqs/graphics/algorithms-faq/
+	"""
+
+
+	j = 1
+	c = 0
+	print c
+	for i in range(len(polygons)):
+		if (((yp[i]<=y) and (y<yp[j])) or \
+			(yp[j]<=y) and (y<yp[i])) and \
+			(x<(xp[j]-xp[i])*(y-yp[i])/(yp[j]-yp[i]) + xp[i]):
+			c = not c
+			print c
+		j = i
+
+	return c 
 
 if __name__ == '__main__':
-	print "here"
 	#init 2D road map as a 40x40 grid
 	map = [(0, 0), (0, 40), (40, 0), (40, 40)]	
 
@@ -91,11 +107,11 @@ if __name__ == '__main__':
 	goal = (random.randint(0, 40), random.randint(0, 40))
 
 	polygons = []
-	polygon = [(2,2), (4,4), (7,7), (10,5), (7,3)]
+	polygon = [(2,2), (4,4), (7,7), (10,5), (7,3), (2,2)]
 	polygons.append(polygon)
-	polygon = [(20,7), (19,15), (28,28), (30,10), (25,6)]
+	polygon = [(20,7), (19,15), (28,28), (30,10), (25,6), (20,7)]
 	polygons.append(polygon)
-	polygon = [(8,17), (2,30), (8,35), (11,30)]
+	polygon = [(8,17), (2,30), (8,35), (11,30), (8,17)]
 	polygons.append(polygon)
 	fig, ax = plt.subplots()
 	verts = [p for p in polygons]
@@ -106,8 +122,11 @@ if __name__ == '__main__':
 
 	path = []
 	path.append(start)
-	print is_inside([32, 15, 0], polygons)
-	sys.exit(0)
+	print polygons[0]
+	xp = [p[0] for p in polygons[0]]
+	yp = [p[1] for p in polygons[0]]
+	#print pnpoly(xp, yp, 30, 5)
+	#print is_inside([35, 5, 0], polygons)
 
 	#Sampling
 	num = 0
@@ -115,21 +134,20 @@ if __name__ == '__main__':
 	while num <50:
 		(x, y) = (random.randint(0, 40), random.randint(0, 40))
 		if not (x, y) in points:
-			if not is_inside([x, y, 0], polygons):
+			if not pnpoly(polygons, x, y):
 				points.append([x, y, 0])
 				gr.add_node(num)
 				num = num + 1
 	plt.plot([x for (x, y, z) in points], [y for (x, y, z) in points], 'ro')
 	plt.show()
-	print gr.nodes()
+	#print gr.nodes()
 
 
 	#connect points to every other point
 	for node in gr.nodes():
 		for node2 in gr.nodes():
 			if node != node2:
-				if not is_inside(points[node], polygons):
-					gr.add_edge(node, node2, weight=get_dist(points[node], points[node2]))
+				gr.add_edge(node, node2, weight=get_dist(points[node], points[node2]))
 
 	sys.exit(0)
 	while cmp(start, goal) != 0:
