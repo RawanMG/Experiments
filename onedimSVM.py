5cb6e8fabf4fb927b409892a251ed1bce44d8dd5
Rawan Alghofaili <rawan.alghofaili@gmail.com>
2014-11-01 19:01:10 -0400

    corrected SVM feature set

--- onedimSVM.py
+++ onedimSVM.py
@@ -15,6 +15,13 @@ def is_between(x1, x2, p):
 	if p < x1 and p > x2:
 		return 1
 	return 0
+def comp_acc(y, y_pred):
+	num_correct = 0
+	for i in range(len(y)):
+		if y[i] == y_pred[i]:
+			num_correct = num_correct + 1
+
+	return 100*(float(num_correct)/len(y))
 
 if __name__ == '__main__':
 	#init line map
@@ -38,9 +45,10 @@ if __name__ == '__main__':
 	while len(x) < train_len:
 		x1 = random.randint(0, len(points))
 		x2 = random.randint(0, len(points))
-		if [x1, x2] not in x:
-			x.append([x1, x2])
-			p = random.randint(0, len(obst))
+		p = random.randint(0, len(obst))
+		if [x1, x2, p] not in x:
+			x.append([x1, x2, p])
+			
 			print  is_between(x1, x2, p)
 			y.append( not is_between(x1, x2, p)) #True if there is a path
 
@@ -55,10 +63,16 @@ if __name__ == '__main__':
 	while len(x_val) < 10:
 		x1 = random.randint(0, len(points))
 		x2 = random.randint(0, len(points))
-		if [x1, x2] not in x_val:
-			x_val.append([x1, x2])
-			y_val.append(clf.predict([x1, x2]))
+		p = random.randint(0, len(obst))
+		if [x1, x2, p] not in x_val:
+			x_val.append([x1, x2, p])
+			y_val.append(clf.predict([x1, x2, p]))
 
-	print y_val
+
+	y = []
+	for   val in x_val:
+		y.append(not is_between(val[0], val[1], val[2]))
+
+	print comp_acc(y, y_val)
 
 
