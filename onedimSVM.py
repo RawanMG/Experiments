6dc48d7ad57b83d567d71f840cbcbbab5ce6e959
Rawan Alghofaili <rawan.alghofaili@gmail.com>
2014-11-09 21:14:13 -0500

    more efficient

--- onedimSVM.py
+++ onedimSVM.py
@@ -26,53 +26,73 @@ def comp_acc(y, y_pred):
 if __name__ == '__main__':
 	#init line map
 	#init obstacles
-	obst = [random.randint(0, 1000000) for i in range(10)]
+	obst = random.randint(0, 3*(10**6)) #[random.randint(0, 1000000) for i in range(10)]
+	print obst
 	#init points
+	sampling_size = 10**3
 	points = []
-	while len(points) <1000:
-		num = random.randint(0, 1000000)
-		if num not in points and num not in obst:
-			points.append(num)
+	for i in xrange(5*(10**2)):
+		num = random.randint(0, 3*(10**6))
+		points.append(num)
 
-	plt.plot(obst, [10 for y in range(len(obst))], 'rx')
+	print len(points)
+	plt.plot(obst, 10, 'rx')
 	plt.plot(points,[10 for y in range(len(points))] ,  'bo')
 	plt.show()
 
-	#create training data
+	#create data set
+	dataset = []
+	for i in range(0, len(points)):
+		for j in range(0, len(points)):
+			if i != j:
+				dataset.append([points[i], points[j]])
+
+
+	#extract training data from dataset
 	x = []
 	y = []
-	train_len = int(len(points)*90.0/100)
+	train_len = int(len(dataset)*90.0/100) #90% of dataset for training
+	print "Creating training set of size = %d" % train_len
+	i = 0
 	while len(x) < train_len:
-		x1 = random.randint(0, len(points))
-		x2 = random.randint(0, len(points))
-		p = random.randint(0, len(obst))
-		if [x1, x2, p] not in x:
-			x.append([x1, x2, p])
-			
-			print  is_between(x1, x2, p)
-			y.append( not is_between(x1, x2, p)) #True if there is a path
+		x1, x2 = dataset[0]
+		p = obst
+		#if [x1, x2] not in x:
+		x.append([x1, x2])	
+		y.append( not is_between(x1, x2, p)) #True if there is a path
+		dataset.pop(0)
+
+
+
+	#validation data
+	print "Creating validation set of size = %d" % len(dataset)
+	x_val = []
+	y_val = []
+	while len(dataset):
+		x1, x2 = dataset[0]
+		p = obst
+		#if [x1, x2] not in x_val:
+		x_val.append([x1, x2])
+		y_val.append(not is_between(x1, x2, p))
+		dataset.pop(0)
 
 
 	#train SVM
+	print "Training SVM"
 	clf = svm.SVC()
 	clf.fit(x, y)
 
-	#validation data
-	x_val = []
-	y_val = []
-	while len(x_val) < 10:
-		x1 = random.randint(0, len(points))
-		x2 = random.randint(0, len(points))
-		p = random.randint(0, len(obst))
-		if [x1, x2, p] not in x_val:
-			x_val.append([x1, x2, p])
-			y_val.append(clf.predict([x1, x2, p]))
+	num_correct = 0
+	for i in range(0, len(y_val)):
+		x1, x2 = x_val[i]
+		y = y_val[i]
+		y_pred = clf.predict([x1, x2])
+		if y_pred == y:
+			num_correct = num_correct + 1
 
 
-	y = []
-	for   val in x_val:
-		y.append(not is_between(val[0], val[1], val[2]))
-
-	print comp_acc(y, y_val)
+	ACC = num_correct /float(len(y_val))
+	print "ACC = %f" %ACC
+	#print comp_acc(y, y_val)
 
 
