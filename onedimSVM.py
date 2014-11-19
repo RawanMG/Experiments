c04624d424342dd624739a50fb92889e18ab7bf5
RawanMG <rawan.alghofaili@gmail.com>
2014-11-19 12:48:13 -0500

    split up training and write to file

--- onedimSVM.py
+++ onedimSVM.py
@@ -7,6 +7,7 @@ from sklearn import svm
 import sys
 
 sys.path.append('/usr/local/lib/python2.7/site-packages')
+sys.path.append('/usr/local/lib')
 import networkx as nx
 
 def is_between(x1, x2, p):
@@ -22,16 +23,16 @@ def comp_acc(y, y_pred):
 			num_correct = num_correct + 1
 
 	return 100*(float(num_correct)/len(y))
-
-if __name__ == '__main__':
+def make_data(filename):
 	#init line map
 	#init obstacles
+	dataset =[]
 	obst = random.randint(0, 3*(10**6)) #[random.randint(0, 1000000) for i in range(10)]
 	print obst
 	#init points
 	sampling_size = 10**3
 	points = []
-	for i in xrange(5*(10**2)):
+	for i in xrange(sampling_size):
 		num = random.randint(0, 3*(10**6))
 		points.append(num)
 
@@ -75,6 +76,32 @@ if __name__ == '__main__':
 		x_val.append([x1, x2])
 		y_val.append(not is_between(x1, x2, p))
 		dataset.pop(0)
+
+	with open('%s_tr.csv'%filename, 'w') as f:
+		for i in range(len(y)):
+			f.write("%d,%d,%d\n"%(y[i], x[i][0], x[i][1]))
+
+	with open('%s_val.csv'%filename, 'w') as f:
+		for i in range(len(y_val)):
+			f.write("%d,%d,%d\n"%(y_val[i], x_val[i][0], x_val[i][1]))
+
+def read_data(filename):
+	y =[]
+	x =[]
+	with open(filename, 'r') as f:
+		for line in f:
+			line = line.split(',')
+			y.append(int(line[0]))
+			x.append([int(line[1]), int(line[2])])
+
+	return y, x
+if __name__ == '__main__':
+
+	make_data("0")
+	y, x = read_data("0_tr.csv")
+	print y
+	print x
+ 	sys.exit(0)
 
 
 	#train SVM
